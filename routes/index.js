302eddc2fa047745d5979aa4aa070dd7c9fd217f
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2015-06-21 13:18:32 +0700

    Can connect to pg

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg'),
+  config = require('../config/config'),
+  connectionString = config.db;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var results = [];
+  console.log(connectionString)
+  pg.connect(connectionString, function(err, client, done) {
+    var query = client.query('SELECT * FROM users limit 10')
+    query.on('row', function(row){
+      results.push(row)
+    });
+    query.on('end', function() {
+      client.end();
+      res.render('index', { title: 'Express', results: results });
+    });
+  })
 });
 
 module.exports = router;
