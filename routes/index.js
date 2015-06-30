b6fc65885f51c741f0db550be8bf7fcdf0a75866
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2015-06-30 15:48:29 +0700

    Only retrieve executedAt from history

--- routes/index.js
+++ routes/index.js
@@ -128,7 +128,8 @@ router.get('/reports(:format?)', function(req, res) {
 });
 
 router.get('/reports/:id/histories', function(req, res) {
-  Report.find({_id: req.params.id}, 'histories', function(err, histories) {
+  Report.find({_id: req.params.id}, 'histories.executedAt', function(err, histories) {
+    console.log(histories)
     res.json({
       data: JSON.stringify(histories[0])
     });
